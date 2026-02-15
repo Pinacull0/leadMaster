dbf4e86e6a6dff96b3d1bac79395888b5cf28633
Pinacull0 <pinacullodevelopment@gmail.com>
2026-02-15 03:20:11 -0300

    bhnijvnrinvgjf

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -19,9 +19,9 @@ function getPool() {
   pool = mysql.createPool({
     host: process.env.DB_HOST!,
     port: Number(process.env.DB_PORT || 3306),
-    user: process.env.DB_USER!,
-    password: process.env.DB_PASSWORD!,
-    database: process.env.DB_NAME!,
+    user: 'ManagerTableProject',
+    password: '2VEYw@#nlpG',
+    database: 'allmanager',
     connectionLimit: Number(process.env.DB_CONNECTION_LIMIT || 10),
     waitForConnections: true,
     queueLimit: 0,
